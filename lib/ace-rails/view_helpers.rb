5c53052b5ff248c826dca0eaf946655d95de55ca
Jean-Baptiste Feldis <jb.feldis@studiomelipone.eu>
2012-02-14 18:52:04 +0100

    Jquery onload compatible

--- lib/ace-rails/view_helpers.rb
+++ lib/ace-rails/view_helpers.rb
@@ -17,14 +17,15 @@ module Ace
   <div class='editor_footer'></div>
 </div>
 <script type='text/javascript'>
-  window.onload = function() {
+  $(function(){
+  //window.onload = function() {
     if (window['ace_editors'] === undefined) window.ace_editors = [];
     
     window.ace_editors['#{id}'] = ace.edit('#{id}');
     #{theme_setter}
     var #{mode_class} = require("ace/mode/#{mode}").Mode;
     window.ace_editors['#{id}'].getSession().setMode(new #{mode_class}());
-  };
+  });
 </script>
 HTML
 
